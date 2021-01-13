da6b72ef178ca16544a8f7e985231fd3cd588f19
skgilmore <skg.gilmore@gmail.com>
2021-01-13 11:45:22 -0600

    added user comps

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@ import { RegisterForm } from "./auth/RegisterForm.js"
 import { Nutshell } from "./Nutshell.js"
 
 
+
 /*
     1. Check if the user is authenticated by looking in session storage for `activeUser`
     2. If so, render the Nutshell component
