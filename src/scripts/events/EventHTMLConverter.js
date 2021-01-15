77735278c9c7bd7d59d81f688d8cfb8cefd80584
jessicadever1 <jessicadever1@gmail.com>
2021-01-15 13:57:50 -0600

    fixed indvidual event cards

--- src/scripts/events/EventHTMLConverter.js
+++ src/scripts/events/EventHTMLConverter.js
@@ -5,16 +5,19 @@ const eventHub = document.querySelector(".container")
 
 export const EventHTMLConverter = (eventObject) => {
     return `
-    <div class="eventCard">
-        <div><h2 class="eventName">${eventObject.name}</h2></div>
-        <div>Date: ${eventObject.date}</div>
-        <div>${eventObject.location}</div>
-        <div>${eventObject.address}</div>
-        <div>${eventObject.city} ${eventObject.state}, ${eventObject.zip}</div>
-        
-        <button class="showWeatherBtn">Show Weather</button>
-    <div>
-    
+    <section class="eventCard">
+        <div>
+            <div><h2 class="eventName">${eventObject.name}</h2></div>
+            <div>Date: ${eventObject.date}</div>
+            <div>${eventObject.location}</div>
+            <div>${eventObject.address}</div>
+            <div>${eventObject.city} ${eventObject.state}, ${eventObject.zip}</div>
+            <div class="eventButtons">
+                <button class="showWeatherBtn">Show Weather</button>
+                <button class="deleteEventBtn">Delete Event</button>
+            </div>
+        <div>
+    </section>
     `
 }
 
